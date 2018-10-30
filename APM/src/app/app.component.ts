18581763beb55715a80abda5da948dc2282748a7
Admin Joel Klingler <AdminJKL@unisg.ch>
2018-10-30 11:44:18 +0100

    11

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: "<div><h1>{{pageTitle}}</h1><div><pm-products></pm-products></div></div>"
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';

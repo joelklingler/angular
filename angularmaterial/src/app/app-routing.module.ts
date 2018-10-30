7a4b6bd90f04db03efef15dee78f1fa4063878ed
Admin Joel Klingler <AdminJKL@unisg.ch>
2018-10-30 16:09:15 +0100

    flexbox demo

---
+++ angularmaterial/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'demo', loadChildren: './demo/demo.module#DemoModule' },
+  { path: '**', redirectTo: 'demo' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

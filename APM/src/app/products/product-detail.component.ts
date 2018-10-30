9fa90c64f1786b07f2848e7b7bc31606c98311c1
Admin Joel Klingler <AdminJKL@unisg.ch>
2018-10-30 12:38:45 +0100

    12

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +11,19 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) {
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProducts().subscribe(x => {
+      this.product = x.find(e => e.productId == id);
+      console.log(this.product)
+    });
   }
 
 }
